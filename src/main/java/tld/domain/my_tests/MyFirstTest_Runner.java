403c4e3e77c4075d9e568644b7b3700fa39ad5a0
Christopher <camenarescoding@gmail.com>
2017-07-30 18:28:47 -0400

    Add a developer token

--- src/main/java/tld/domain/my_tests/MyFirstTest_Runner.java
+++ src/main/java/tld/domain/my_tests/MyFirstTest_Runner.java
@@ -1,5 +1,8 @@
 package tld.domain.my_tests;
 
 public class MyFirstTest_Runner {
+	
+	// Setting the development token
+	private final static String devToken = "fW7hn0jEvJXASPoM1jK5IauH0ebp081";
 
 }
