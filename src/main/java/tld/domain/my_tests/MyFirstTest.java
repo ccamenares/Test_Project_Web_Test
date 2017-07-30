b2d2cb69594e12d6417e3c210bbfb3c1d390d4fa
Christopher <camenarescoding@gmail.com>
2017-07-30 17:20:38 -0400

    Create WebDriver object

--- src/main/java/tld/domain/my_tests/MyFirstTest.java
+++ src/main/java/tld/domain/my_tests/MyFirstTest.java
@@ -1,5 +1,7 @@
 package tld.domain.my_tests;
 
+import org.openqa.selenium.WebDriver;
+
 import io.testproject.java.enums.ExecutionResultType;
 import io.testproject.java.sdk.generated.codeblocks.Test;
 
@@ -8,6 +10,11 @@ public class MyFirstTest extends Test {
 	@Override
 	protected ExecutionResultType execute() throws Exception {
 		// TODO Auto-generated method stub
+		
+		// Get driver initialized by TestProject  Agent
+		// No need to specify browser type, it can be done later via UI
+		WebDriver  driver  =  this.getWebDriver();
+		
 		return null;
 	}
 
